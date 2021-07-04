6dc71930f673fc3c4964ef7a1233a8d70c7848e6
EdenN0 <EdenN0>
2021-07-04 14:09:59 +0900

    Initial Commit

---
+++ miyagi_coupon/miyagi_couponApp.swift
@@ -0,0 +1,17 @@
+//
+//  miyagi_couponApp.swift
+//  miyagi_coupon
+//
+//  Created by EdenN0 on 2021/07/04.
+//
+
+import SwiftUI
+
+@main
+struct miyagi_couponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
